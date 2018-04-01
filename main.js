d27eff80a6a516e93d52022d6749776201a3f78c
Pablo Padilla <pgpb.padilla@gmail.com>
2018-04-01 15:28:56 -0700

    chg: usr: Send message in text are to chat server.

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
 (function () {
-    console.log('Hello');
-    // Create WebSocket connection.
-    const socket = new WebSocket('ws://localhost:8765');
 
-    // Connection opened
-    socket.addEventListener('open', function (event) {
-        socket.send('Hello Server!');
-    });
 
-    // Listen for messages
-    socket.addEventListener('message', function (event) {
-        console.log('Message from server ', event.data);
+
+    function setupConnection() {
+        const socket = new WebSocket('ws://localhost:8765');
+        socket.addEventListener('open', function (event) {
+            socket.send('Hello Server!');
+        });
+        socket.addEventListener('message', function (event) {
+            console.log('Message from server ', event.data);
+        });
+        return socket;
+    }
+
+    function setupUIEvents(socket) {
+        let sendBtn = document.querySelector('.compose button');
+        sendBtn.addEventListener('click', (e) => {
+            let messageBox = document.querySelector('.compose .message');
+            let text = messageBox.value;
+            console.log('Sent message', text);
+            socket.send(text);
+        });
+    }
+
+    document.addEventListener("DOMContentLoaded", function(event) {
+        console.log("DOM fully loaded and parsed");
+        const socket = setupConnection();
+        setupUIEvents(socket);
     });
 })()

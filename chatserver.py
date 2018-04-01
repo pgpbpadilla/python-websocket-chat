f4dc4a22c7409b4bd39a993c88854a3f530a15d8
Pablo Padilla <pgpb.padilla@gmail.com>
2018-04-01 15:08:25 -0700

    chg: dev: Basic echo server + client code to open the socket.

--- chatserver.py
+++ chatserver.py
@@ -1,38 +1,11 @@
-"""The most basic chat protocol possible.
-
-run me with twistd -y chatserver.py, and then connect with multiple
-telnet clients to port 1025
-"""
-from __future__ import print_function
-
-from twisted.protocols import basic
+import asyncio
+import websockets
 
 
+async def echo(websocket, path):
+    async for message in websocket:
+        await websocket.send(message)
 
-class MyChat(basic.LineReceiver):
-    def connectionMade(self):
-        print("Got new client!")
-        self.factory.clients.append(self)
-
-    def connectionLost(self, reason):
-        print("Lost a client!")
-        self.factory.clients.remove(self)
-
-    def lineReceived(self, line):
-        print("received", repr(line))
-        for c in self.factory.clients:
-            c.message(line)
-
-    def message(self, message):
-        self.transport.write(message + b'\n')
-
-
-from twisted.internet import protocol
-from twisted.application import service, internet
-
-factory = protocol.ServerFactory()
-factory.protocol = MyChat
-factory.clients = []
-
-application = service.Application("chatserver")
-internet.TCPServer(1025, factory).setServiceParent(application)
+asyncio.get_event_loop().run_until_complete(
+    websockets.serve(echo, 'localhost', 8765))
+asyncio.get_event_loop().run_forever()
